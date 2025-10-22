f0fb062f247bb3420c1ebffb5f653b37b063bffa
bhaskar <raibhaskar003@gmail.com>
2025-10-22 15:24:32 +0545

    Prediction pipeline

--- semo.py
+++ semo.py
@@ -1,2 +1,3 @@
 import os 
 print(os.path.dirname("ml_project.egg-info/dependency_links.txt"))
+
--- src/utils.py
+++ src/utils.py
@@ -14,7 +14,7 @@ def save_object(file_path,obj):
         os.makedirs(dir_path,exist_ok=True)
 
         with open(file_path,'wb') as file_obj:
-            dill.dump(obj,file_obj)
+            dill.dump(obj,file_obj) # saving file in bytes file 
     except Exception as e:
         raise CustomException(e,sys)
     
@@ -31,19 +31,20 @@ def evaluate_model(x_train,y_train,x_test,y_test,models):
             # y_train_pred = model.predict(x_test)
             y_test_pred = model.predict(x_test)
 
-            # # for train data
-            # score_train = r2_score(y_train,y_train_pred)
-            # mae_train = mean_absolute_error(y_train,y_train_pred)
-            # mse_train = mean_squared_error(y_train,y_train_pred)
-            # rmse_train = root_mean_squared_error(y_train,y_train_pred)
-
             # for test data
             score_test = r2_score(y_test,y_test_pred)
-            # mea_test = mean_absolute_error(y_test,y_test_pred)
-            # mse_test = mean_squared_error(y_test,y_test_pred)
-            # rmse_test = root_mean_squared_error(y_test,y_test_pred)
             report[m] = score_test
 
             return report
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+# load the pkl file and return the object 
+def load_model_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+
